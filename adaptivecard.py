36b40b80e24f0d338b8e2a7b268d373ed0063847
isalazar4826 <31516046+isalazar4826@users.noreply.github.com>
2020-03-26 16:10:46 -0600

    Update adaptivecard.py

--- adaptivecard.py
+++ adaptivecard.py
@@ -5,15 +5,6 @@ from webexteamssdk import *
 from flask import Flask, request
 app = Flask(__name__)
 
-'''
-For prototyping, we use ngrok.
-We'll request the tunnel and parse the url to use for a webhook
-'''
-tunnel = json.loads(
-    requests.request('GET', url='https://adaptuvecardweex.herokuapp.com/'
-                     ).text
-)
-public_url = tunnel['tunnels'][0]['public_url']
 
 '''
 Specify the webex token and roomId to use
